3ed960b326272e5c40b46444c8039e4f0a54002b
shecho <shechovala@gmail.com>
2020-07-14 21:55:10 -0500

    more stuff

--- src/App.js
+++ src/App.js
@@ -6,85 +6,110 @@ class App extends React.Component {
     super();
     this.state = {
       users: [],
+      newUser: {},
     };
   }
 
   componentDidMount() {
-   
-    this.getData() 
+    this.getData();
     // fetch('https://academlo-api-users.herokuapp.com/users')
     // .then(res => res.json())
     // .then(datos =>console.log(datos))
-  
   }
   getData = async () => {
-    let response = await fetch("https://academlo-api-users.herokuapp.com/users");
-    let data =await  response.json();
-    // console.log(data);
-    this.setState({ users: data.data })
-    console.log(this.state.users)
-  }
+    let response = await fetch(
+      "https://academlo-api-users.herokuapp.com/users"
+    );
+    let data = await response.json();
+    console.log(data);
+    this.setState({ users: data.data });
+    // console.log(this.state.users)
+  };
 
-  // addUser = (e) => {
-    // console.log(e);
-  //   event.preventDefault();
-  //   //Agregar un post
-  //   fetch("https://academlo-api-users.herokuapp.com/users", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json; charset=UTF-8",
-  //     },
-  //     body: JSON.stringify(),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((results) => console.log(results))
-  //     .catch((error) => console.log(error));
-  // };
+  addUser = (e) => {
+    console.log(e);
+    e.preventDefault();
+    // //Agregar un post
+      fetch("https://academlo-api-users.herokuapp.com/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(this.state.newUser),
+      })
+        .then((response) => response.json())
+        .then((results) => console.log(results))
+        .catch((error) => console.log(error));
+  };
 
   handleInput = (e) => {
+    // let newUser = e.target.value;
     console.log(e.target.value);
+    this.setState({
+      newUser:{
+        ...this.state.newUser,
+          [e.target.name]:e.target.value
+      }
+    })
+     
   };
 
   render() {
     return (
       <>
-       <div className ='m-4 row'>
-       <form>
-          <div className="form-group">
-            <input className="form-control" name="title" type="text" placeholder="Nombre" />
-          </div>
-          
-          <div className="form-group">
-            <input className="form-control" name="body" type="text" placeholder="Apellido" />
-          </div>
-          <div className="form-group"> 
-            <input className="form-control" name="body" type="email" placeholder="Email" />
-          </div>
-          <div className="form-group">
-            <input className="form-control" name="body" type="password" placeholder="Password" />
-          </div>
-          <div className="form-group">
-            <input type="submit" />
-          </div>
-          
-         </form>
+        <div className="m-4 row">
+          <form onInput={this.handleInput}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                name="nombre"
+                type="text"
+                placeholder="Nombre"
+              />
+            </div>
 
-       </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                name="apellido"
+                type="text"
+                placeholder="Apellido"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                name="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-group">
+              <input type="submit" />
+            </div>
+          </form>
+        </div>
 
         {this.state.users.map((usuairos) => {
-            return ( 
-              
-              <div className="card" key={usuairos.email}>
-                <div className="card-body"> 
-                  <h1 className="card-title">{usuairos.name} {usuairos.lastname}</h1>
-                  <p className="card-text">{usuairos.email}</p>
-                </div>
+          return (
+            <div className="card" key={usuairos.email}>
+              <div className="card-body">
+                <h1 className="card-title">
+                  {usuairos.name} {usuairos.lastname}
+                </h1>
+                <p className="card-text">{usuairos.email}</p>
               </div>
-              
-            );
-          })}
-
-       
+            </div>
+          );
+        })}
       </>
     );
   }
